331cb87c8e6022bdc4cf0b0c9e4c9b54e36c0887
Will <doggy.huang@gmail.com>
2016-06-29 23:18:14 +0800

    撰寫 NgStyle 範例程式
    https://angular.cn/docs/ts/latest/guide/template-syntax.html#!#ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  isSpecial = true;
+  color = "#FF0000";
+
+  canSave = true;
+  isUnchanged = true;
+
+  setStyles() {
+    let styles = {
+      // CSS property names
+      'font-style':  this.canSave      ? 'italic' : 'normal',  // italic
+      'font-weight': !this.isUnchanged ? 'bold'   : 'normal',  // normal
+      'font-size':   this.isSpecial    ? '24px'   : '8px',     // 24px
+    };
+    return styles;
+  }
 }
