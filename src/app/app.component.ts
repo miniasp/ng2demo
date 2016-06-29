1c77d22bb31ccc393e00a1d015220d52251825c1
Will <doggy.huang@gmail.com>
2016-06-29 23:30:57 +0800

    撰寫 NgClass 範例程式
    https://angular.cn/docs/ts/latest/guide/template-syntax.html#!#ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  isSpecial = true;
+
+  canSave = true;
+  isUnchanged = true;
+
+  setClasses() {
+    let classes =  {
+      saveable: this.canSave,      // true
+      modified: this.isUnchanged,  // true
+      special: !this.isSpecial,    // false
+    };
+    return classes;
+  }
 }
