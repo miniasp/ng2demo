aef66085638e27de01a5b3481540921b2fb80ed1
Will <doggy.huang@gmail.com>
2016-06-29 23:43:33 +0800

    撰寫 NgIf 範例程式
    https://angular.cn/docs/ts/latest/guide/template-syntax.html#!#ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'app works!';
+
+  isActive = false;
+
+  currentHero = {
+    firstName: 'Will'
+  };
+
+  isSpecial = true;
+
 }
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hero-detail',
+  templateUrl: '/app/hero-detail/hero-detail.component.html'
+})
+export class HeroDetailComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}
